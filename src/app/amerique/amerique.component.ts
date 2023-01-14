09922587f9f757b2fc65dce1a7c61552f1b1f27a
Mamadou siradio Barry <siradio156@gmail.com>
2023-01-14 15:00:39 +0100

    ajout des continents

--- src/app/amerique/amerique.component.ts
+++ src/app/amerique/amerique.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {destination} from "../model/destination.model";
+import {DestinationService} from "../services/destination.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-amerique',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./amerique.component.scss']
 })
 export class AmeriqueComponent {
+  @Input() destinations!: destination[];
+
+  constructor(private destinationService: DestinationService, public router: Router) {
+  }
+  ngOnInit():void {
+    this.destinations=this.destinationService.getAmeriqueDestinations();
+  }
 
 }
