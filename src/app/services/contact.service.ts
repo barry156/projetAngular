49dda0efadd6aea62cbb03372080aab97cc47263
Tony Grimm <uvoix66@hotmail.com>
2023-01-15 01:26:41 +0100

    quelques images rajoutées et hiearchie de fichiers assets crée

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -12,32 +12,24 @@ export class ContactService{
     {
 
       id:1,
-      nomLieu:'Yaounde',
-      pays: 'cameroun',
-      imageUrl:'assets/img_3.png',
-      region: 'centre',
       nomPersonne:'Mamadou',
       prenomPersonne: 'Barry',
       identifiantIG: 'itsBarry',
       numTel: 145,
       facebook: 'Aucun',
-      photoUrl:'assets/img_3.png',
+      photoUrl:'assets/contacts/img_3.png',
       email:'siradio156@gmail.com',
       activites:"fleuve"
     },
 
     {
       id:2,
-      nomLieu:'Yaounde',
-      pays: 'cameroun',
-      imageUrl:'assets/img2.jpg',
-      region: 'centre',
       nomPersonne:'Nguemnang',
       prenomPersonne: 'Naomie',
       identifiantIG: 'itsnaomie',
       numTel: 145,
       facebook: 'Aucun',
-      photoUrl:'assets/img2.jpg',
+      photoUrl:'assets/contacts/img2.jpg',
       email:'naomienguemnang@gmail.com',
       activites:"fleuve"
 
@@ -45,16 +37,12 @@ export class ContactService{
 
     {
       id:3,
-      nomLieu:'Yaounde',
-      pays: 'cameroun',
-      imageUrl:'assets/img_2.png',
-      region: 'centre',
       nomPersonne:'Linemann',
       prenomPersonne: 'Anton',
       identifiantIG: 'itsAnton',
       numTel: 145,
       facebook: 'Aucun',
-      photoUrl:'assets/img_2.png',
+      photoUrl:'assets/contacts/img_2.png',
       email:'anton.linemann@hotmail.com',
       activites:"fleuve"
     }
